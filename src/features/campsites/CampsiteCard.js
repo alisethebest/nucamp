59d79501777b4948b9c3f17f1fee08cd07dd0588
Alise Cervantes <alise09@icloud.com>
2023-09-16 08:24:10 -0700

    nucamp

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
-const CampsiteCard = (props) => {
-  return (
-    <Card>
-      <CardImg
-        width="100%"
-        src={props.campsite.image}
-        alt={props.campsite.name}
-      />
-      <CardImgOverlay>
-        <CardTitle>{props.campsite.name}</CardTitle>
-      </CardImgOverlay>
-    </Card>
-  );
+const CampsiteCard = ({ campsite }) => {
+    const { image, name } = campsite;
+    return (
+        <Card>
+            <CardImg width='100%' src={image} alt={name} />
+            <CardImgOverlay>
+                <CardTitle>{name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
 };
 
 export default CampsiteCard;
